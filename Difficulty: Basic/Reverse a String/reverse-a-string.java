46b76ae8120f347c29e860a01c30a3cd3198ad7b
Bharadwaj292007 <kandunuribharadwaj55@gmail.com>
2026-07-11 12:12:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static String reverseString(String s) {
+        // code here
+        StringBuilder res=new StringBuilder();
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            res.append(s.charAt(i));
+        }
+        return res.toString();
+        
+    }
+}
