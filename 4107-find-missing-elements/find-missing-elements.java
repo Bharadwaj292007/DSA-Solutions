9c69827066157206775fc9c941faba0ef08eac64
Bharadwaj292007 <kandunuribharadwaj55@gmail.com>
2026-08-10 19:54:45 +0530

    Time: 4 ms (48.94%) | Memory: 46.5 MB (94.68%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int min =Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>max)
+            {
+                max=nums[i];
+            }
+            if(nums[i]<min)
+            {
+                min=nums[i];
+            }
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=min;i<=max;i++)
+        {
+            list.add(i);
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            list.remove(Integer.valueOf(nums[i]));
+        }
+    return list;
+    }
+}
